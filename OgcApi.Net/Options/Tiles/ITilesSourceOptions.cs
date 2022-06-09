6bfb35177710100b529e0cdf8a9a3b5c6b77ff9f
sask <sask@SASK>
2022-06-09 16:57:54 +0400

    Added comment  for TileAccessDelegate type

--- OgcApi.Net/Options/Tiles/ITilesSourceOptions.cs
+++ OgcApi.Net/Options/Tiles/ITilesSourceOptions.cs
@@ -2,6 +2,15 @@
 
 namespace OgcApi.Net.Options.Tiles
 {
+    /// <summary>
+    /// Returns true when access to tile is granted according to the given parameters, otherwise returns false
+    /// </summary>
+    /// <param name="collectionId"></param>
+    /// <param name="tileMatrix"></param>
+    /// <param name="tileRow"></param>
+    /// <param name="tileCol"></param>
+    /// <param name="apiKey"></param>
+    /// <returns></returns>
     public delegate bool TileAccessDelegate(string collectionId, int tileMatrix, int tileRow, int tileCol, string apiKey);
     public interface ITilesSourceOptions
     {
